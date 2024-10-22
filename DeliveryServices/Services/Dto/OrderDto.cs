afa69efd39fb364cd50ed8c4f5b3694a6bf62532
Dedukhin Alexandr <vyshky2@gmail.com>
2024-10-22 21:27:39 +0400

    Добавьте файлы проекта.

---
+++ DeliveryServices/Services/Dto/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryServices.Services.Dto
+{
+    internal class OrderDto
+    {
+        public Guid Id { get; set; }
+        public float Weight { get; set; }
+        public string District { get; set; }
+        public DateTime DeliveryTime { get; set; }
+    }
+}
---
+++ DeliveryServices/Services/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using DeliveryServices.Domain.Repository;
+using DeliveryServices.Services.Dto;
+using System.Data;
+using System.Globalization;
+
+namespace DeliveryServices.Service
+{
+    internal class OrderService : IOrderService
+    {
+        IOrderRepository orderRepository;
+        public OrderService(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+        public List<OrderDto> FilterOrdersByDistrictAndTime(string district, string format, string begin, string end)
+        {
+            DateTime startDateTime = DateTime.ParseExact(begin, format, CultureInfo.InvariantCulture);
+            DateTime endDateTime = DateTime.ParseExact(end, format, CultureInfo.InvariantCulture);
+            if (startDateTime >= endDateTime)
+            {
+                throw new ArgumentException($"Параметр {nameof(begin)}:{begin} должен быть меньше {nameof(end)}:{end}");
+            }
+            return orderRepository.FilterOrdersByDistrictAndTime(district, startDateTime, endDateTime);
+        }
+
+    }
+}
